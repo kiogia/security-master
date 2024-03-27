a5d4b2881e7b97ba5a5a79b5a18711eef5645e4a
kiogia <kiogia.dev@gmail.com>
2024-03-27 10:08:51 +0400

    {version}

--- module/security-master.js
+++ module/security-master.js
@@ -1,10 +1,10 @@
 import dictionary from './dictionary.js';
 
-export * as dictionary from './dictionary.js';
-
-export function createPhrase(length = 12) {
+function createPhrase(length = 12) {
   const mixed = dictionary.sort(() => 0.5 - Math.random());
   return mixed.slice(0, length);
 }
+
+export { dictionary, createPhrase };
 
 export default { dictionary, createPhrase };
